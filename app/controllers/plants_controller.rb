a892c8c7be2a0ebe3a93b638005617e73bd60b75
kenken524 <ken5240719@gmail.com>
2023-09-03 00:50:19 +0900

    ユーザー、植物のアソシエーション設定

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,4 +2,12 @@ class PlantsController < ApplicationController
   def index
     @plants = Plant.all
   end
+
+  private
+
+  def plants_params
+    params.require(:plant).permit(:name, :description, :start_date, :image).merge(user_id: current_user.id)
+  end
+
+  
 end
