3e1b8588cd773e0e6913b173944984010b6eb6ea
kenken524 <ken5240719@gmail.com>
2023-09-03 00:20:07 +0900

    トップページの作成

--- spec/factories/plants.rb
+++ spec/factories/plants.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
   factory :plant do
-    
   end
 end
--- spec/requests/plants_request_spec.rb
+++ spec/requests/plants_request_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
-RSpec.describe "Plants", type: :request do
-
+RSpec.describe 'Plants', type: :request do
 end
