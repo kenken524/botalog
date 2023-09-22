a1b2a11437ddaf3bd7a80d5fc6977e01fbd99548
kenken524 <ken5240719@gmail.com>
2023-09-22 10:22:53 +0900

    ログイン後の遷移先設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_current_location, unless: :devise_controller?
 
   private
 
@@ -14,4 +15,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :profile, :image])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :profile, :image])
   end
+
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+  end
+
 end
